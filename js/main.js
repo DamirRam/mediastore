43f0296aab049e378ea589f1cf23a664d84abbe8
DamirRam <59686045+DamirRam@users.noreply.github.com>
2020-08-23 22:50:27 +0300

    переделал скрипт для internet explorer,откдючил анимацию кнопок в internet explorer

--- js/main.js
+++ js/main.js
@@ -1,60 +1,59 @@
-window.addEventListener("load", function () {
+window.addEventListener("load", function() {
+  //closest
+  !function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
   //модальные окна
   //отключение прокрутки при всплытии модального окна
-  var body = document.body;
-  var offsetTop;
+  let body = document.body;
+  let offsetTop;
 
-  function noScroll(modal) {
-    offsetTop = window.pageYOffset;
-    var scrollBarWidth = window.innerWidth - body.offsetWidth;
-    modal.style.overflowY = "scroll";
+  function noScroll (modal) {
+    offsetTop          = window.pageYOffset;
+    let scrollBarWidth = window.innerWidth-body.offsetWidth;
+    modal.style.overflowY ="scroll";
     body.style.overflowY = "hidden";
-    body.style.top = "-" + offsetTop + "px";
-    body.style.right = scrollBarWidth + "px";
+    body.style.top = "-"+offsetTop+"px";
+    body.style.right = scrollBarWidth+"px";
     body.style.position = "fixed";
-  }
+  };
 
-  function scroll(modal) {
-    modal.style.overflowY = "hidden";
+  function scroll (modal) {
+    modal.style.overflowY="hidden"
     body.style.position = "static";
     body.style.right = "0px";
     body.style.right = "0px";
-    body.style.overflowY = "scroll";
+    body.style.overflowY ="scroll";
     window.scroll(0, offsetTop);
-  }
+  };
 
   //открытие и закрытие модальных окон
-  var openBtns = document.querySelectorAll(".js-open-modal");
-  var closeBtns = document.querySelectorAll(".js-close-modal");
+  let openBtns  = document.querySelectorAll(".js-open-modal");
+  let closeBtns = document.querySelectorAll(".js-close-modal");
 
-  var _loop = function _loop(i) {
-    openBtns[i].addEventListener("click", function (event) {
-      event.preventDefault();
-      var modalId = openBtns[i].getAttribute("data-modal");
-      var modal = document.querySelector(
-        ".modal[data-modal='" + modalId + "']"
-      );
-      modal.classList.add("active");
-      modal.querySelector(".modal__content").classList.add("active");
-      noScroll(modal);
-    }); //end addEventListener
-  };
+  for(let i=0; i<openBtns.length; i++){
+      openBtns[i].addEventListener("click", function (event) {
+        event.preventDefault();
+        let modalId = this.getAttribute("data-modal");
+        let modal   = document.querySelector(".modal[data-modal='"+modalId+"']");
+        modal.classList.add("active");
+        modal.querySelector(".modal__content").classList.add("active");
+        noScroll(modal);
+      });//end addEventListener
+  };//end for
 
-
-  for (var i = 0; i < openBtns.length; i++) {
-    _loop(i);
-  } //end for
-
-  var _loop2 = function _loop2(_i) {
-    closeBtns[_i].addEventListener("click", function (event) {
-      var modal = closeBtns[_i].closest(".modal");
-      closeBtns[_i].closest(".modal__content").classList.remove("active");
+  for(let i=0; i<closeBtns.length; i++){
+    closeBtns[i].addEventListener("click", function (event) {
+      let modal = this.closest(".modal");
+      this.closest(".modal__content").classList.remove("active");
       modal.classList.remove("active");
       scroll(modal);
-    }); //end addEventListener
+    });//end addEventListener
   };
-
-  for (var _i = 0; _i < closeBtns.length; _i++) {
-    _loop2(_i);
-  } //скрытие виджета spikme
+  // отключение анимации кнопки с internet explorer
+  let buttons = document.querySelectorAll("button");
+  for(let i=0; i<buttons.length; i++) {
+    buttons[i].addEventListener("mousedown", function (event){
+    event.preventDefault();
+    console.log("click");
+  });//end addEventListener
+  }//end for
 });
